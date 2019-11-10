238683cc214e66a353c825f001e2f6576a3ba6c2
IryArkhy <37odri@gmail.com>
2019-11-10 15:44:05 +0200

    Updates

---
+++ precache-manifest.1fb14e24639c920709ca7998e91e7582.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "916e7aeef0a58140b1e6a9b0f7c49e2f",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "723c6b7177179202ff12",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.568dd9e7.chunk.css"
+  },
+  {
+    "revision": "eb429daea12b72b5e96f",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.d827b078.chunk.css"
+  },
+  {
+    "revision": "723c6b7177179202ff12",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.789edec5.chunk.js"
+  },
+  {
+    "revision": "eb429daea12b72b5e96f",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.c6c6a644.chunk.js"
+  },
+  {
+    "revision": "044cc51c750f86f0a1e5",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.8f298cfe.js"
+  }
+]);
